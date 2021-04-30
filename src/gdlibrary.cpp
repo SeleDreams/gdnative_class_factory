9ee2d6d3e3eb390323069583dff93b122ddb8835
seledreams <seledreams@pop-os.localdomain>
2021-04-30 07:23:58 +0200

    Added check for editor to make sure the gdns generation only occurs in editor mode and not in play mode

--- src/gdlibrary.cpp
+++ src/gdlibrary.cpp
@@ -1,4 +1,5 @@
 #include "ClassFactoryManager.h"
+#include <Engine.hpp>
 
 extern "C" void GDN_EXPORT godot_gdnative_init(godot_gdnative_init_options *o) {
     godot::Godot::gdnative_init(o);
@@ -11,6 +12,11 @@ extern "C" void GDN_EXPORT godot_gdnative_terminate(godot_gdnative_terminate_opt
 extern "C" void GDN_EXPORT godot_nativescript_init(void *handle) {
     godot::Godot::nativescript_init(handle);
     ClassFactoryManager::register_classes();
+    bool editor = godot::Engine::get_singleton()->is_editor_hint();
+    if (!editor)
+    {
+        return;
+    }
     ClassFactoryManager::generate_gdns("bin","gdnative_class_factory"); // you can remove this if you don't want the gdns files to get generated
                                                                         // make sure this function only gets called in editor, as it might produce unwanted 
                                                                         // results in builds
